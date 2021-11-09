0ed15a12ad3a7b2f41c9354d858c8231ccbd3780
generalm88 <generalm88@gmail.com>
2021-11-10 01:01:27 +0300

    Fix: add text wrapping

--- src/components/TodoItemCard/TodoItemCard.tsx
+++ src/components/TodoItemCard/TodoItemCard.tsx
@@ -16,6 +16,7 @@ const useTodoItemCardStyles = makeStyles({
   root: {
     marginTop: 24,
     marginBottom: 24,
+    wordBreak: "break-all",
   },
   doneRoot: {
     textDecoration: "line-through",
